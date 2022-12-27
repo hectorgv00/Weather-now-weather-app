af4de87f2f8d4b99950f123734004c1f65c907cd
hector.gv00 <hector.gv00@gmail.com>
2022-12-27 17:00:34 +0100

    [feat]: data is arriving correctly to the dashboard. Mapping has not been implemented yet

--- src/Containers/Dashboard/Dashboard.jsx
+++ src/Containers/Dashboard/Dashboard.jsx
@@ -1,22 +1,52 @@
 import React, { useEffect, useState } from 'react';
 import BarChart from '../../Components/BarChart/BarChart';
-import { getApi } from '../../Services/Services';
+import { getApiWeather, getApiForectast } from '../../Services/Services';
 
 function Dashboard(props) {
 
-     const [data, setdata] = useState([])
+     const [weatherData, setWeatherData] = useState([])
+     const [forecastData, setForecastData] = useState([])
+     const [isLoading, setIsLoading] = useState(true);
+     const [flag, setFlag] = useState(true)
+
 
     useEffect(()=> {
-        getApi("Valencia,spain").then(data => setdata(data))
-        console.log(data);
-    })
+        getApiWeather("Valencia,spain").then(data => setWeatherData(data)).then(setIsLoading(false))
+        ;
+    },[flag])
+
+    useEffect(()=>{
+        getApiForectast("Valencia,spain").then(data => setForecastData(data)).then(setIsLoading(false))
+    },[flag])
+
+    if (isLoading) {
+        return(
+        <div className='d-flex vh-100 align-items-center justify-content-center'>
+
+        <div className="spinner-border text-primary" role="status">
+        <span className="sr-only"></span>
+      </div>
+        </div>
+      )
+    } else {
+
+        return (
+    
+    
+    
+            <div className='vh-100 d-flex align-items-center justify-content-center bg-white2'>
+                {/* <BarChart data={forecastData} options = {"o"} /> */}
+                <div>
+                    <p>
+                        
+                  Eyey
+                        </p>
+                </div>
+            </div>
+        );
+    }
 
 
-    return (
-        <>
-            {/* <BarChart data={"eyey"} options = {"oyoy"} /> */}
-        </>
-    );
 }
 
 export default Dashboard;
--- src/Services/Services.js
+++ src/Services/Services.js
@@ -1,15 +1,23 @@
-import axios from "axios"
+import axios from "axios";
 
-const API_URL_FORECAST = "http://api.openweathermap.org/data/2.5/forecast?q=" 
-const API_URL_WEATHER = "http://api.openweathermap.org/data/2.5/weather?q=" 
-const API_KEY = "&appid=cdfd0d9a7c1f33c293edfaeef24004db"
+const API_URL_FORECAST = "http://api.openweathermap.org/data/2.5/forecast?q=";
+const API_URL_WEATHER = "http://api.openweathermap.org/data/2.5/weather?q=";
+const API_KEY = "&appid=cdfd0d9a7c1f33c293edfaeef24004db";
 
-export const  getApi = async (place) =>{
-    try{
-
-        let resp = await axios.get(`${API_URL_WEATHER}${place}${API_KEY}`);
-        console.log(resp)
-    } catch(error){
-        console.log(error)
-    }
+export const getApiWeather = async (place) => {
+  try {
+    let resp = await axios.get(`${API_URL_WEATHER}${place}${API_KEY}`);
+    return resp
+  } catch (error) {
+    console.log(error);
+  }
 }
+;
+export const getApiForectast = async (place) => {
+  try {
+    let resp = await axios.get(`${API_URL_FORECAST}${place}${API_KEY}`);
+    return resp
+  } catch (error) {
+    console.log(error);
+  }
+};

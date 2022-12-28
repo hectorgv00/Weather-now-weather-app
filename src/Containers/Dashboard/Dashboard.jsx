a8c4abc4993941111eb155f3a7490c52f21ccc1d
hector.gv00 <hector.gv00@gmail.com>
2022-12-28 12:39:59 +0100

    [structure]: moved logic to Dashboard

--- src/Containers/Dashboard/Dashboard.jsx
+++ src/Containers/Dashboard/Dashboard.jsx
@@ -1,57 +1,47 @@
 import React, { useEffect, useState } from "react";
+import "./Dashboard.css"
 import BarChart from "../../Components/BarChart/BarChart";
-import { getApiWeather, getApiForectast } from "../../Services/Services";
+import LineChart from "../../Components/LineChart/LineChart";
+import { getApiForectast } from "../../Services/Services";
 
 function Dashboard(props) {
+
+  const [forecastData, setForecastData] = useState([]);
+
 
   const city = "Madrid,spain"
 
 
-  // useEffect(() => {
-  //   getApiWeather("Valencia,spain")
-  //     .then((data) => setWeatherData(data))
-  //     .then(setIsLoadingWeather(false));
-  // }, [flag]);
+  useEffect(() => {
+    getApiForectast(city)
+      .then((data) => setForecastData(data))
+  }, [city]);
 
-  // useEffect(() => {
-  //   getApiForectast("Valencia,spain")
-  //     .then((data) => setForecastData(data))
-  //     .then(setIsLoadingForecast(false));
-  // }, [flag]);
 
-  // if (isLoadingWeather || isLoadingForecast) {
-  //   return (
-  //     <div className="d-flex vh-100 align-items-center justify-content-center">
-  //       <div className="spinner-border text-primary" role="status">
-  //         <span className="sr-only"></span>
-  //       </div>
-  //     </div>
-  //   );
-  // } else {
     return (
       <div className="container-fluid vh-100">
         <div className="row">
           
-          <div className="col-md-6 d-flex align-items-center justify-content-center bg-white2">
-              <BarChart city={city} />
+          <div className="col-md-6 vh-40 d-flex align-items-center justify-content-center bg-white2">
+              <BarChart city={city} forecastData={forecastData} />
           </div>
           
 
-          <div className="col-md-6 vh-25 d-flex align-items-center justify-content-center bg-white2">
-              <BarChart city={city} />
+          <div className="col-md-6 vh-40  d-flex align-items-center justify-content-center bg-white2">
+            <LineChart city = {city} forecastData={forecastData} className="percentage100"/>
           </div>
           
         </div>
 
         <div className="row">
           
-          <div className="col-md-6 d-flex align-items-center justify-content-center bg-white2">
-              <BarChart city={city} />
+          <div className="col-md-6 vh-40 d-flex align-items-center justify-content-center bg-white2">
+              <BarChart city={city}  forecastData={forecastData} />
           </div>
           
 
-          <div className="col-md-6 vh-25 d-flex align-items-center justify-content-center bg-white2">
-              <BarChart city={city} />
+          <div className="col-md-6 vh-40 d-flex align-items-center justify-content-center bg-white2">
+              <BarChart city={city}  forecastData={forecastData} />
           </div>
           
         </div>
